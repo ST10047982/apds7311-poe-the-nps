3e61818e10d89cdbb0ae9b661cbcef29c56087cb
PaigeRussell <104575754+ST10193265@users.noreply.github.com>
2024-11-11 07:38:20 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,9 +19,9 @@ connectDB();
 app.use(helmet()); // Security headers
 app.use(express.json()); // Parse JSON bodies
 app.use(morgan('combined')); // Log HTTP requests
-// app.use(cors({
-//     origin: 'http://localhost:3000', 
-app.use(cors({}));
+ app.use(cors({
+    origin: 'http://localhost:3000'
+ },
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', transactionRoutes);
